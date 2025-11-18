63f572a00946b7dae986c976af6d92a16d6cdd1b
ASWANI K A <aswanika05@gmail.com>
2025-11-18 18:55:55 +0530

    feat: add TodoItem component and apply Tailwind styling to the component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -24,7 +24,7 @@ function TodoForm() {
         />
         <button 
         type="submit"
-        className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'
+        className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0 cursor-pointer'
         >Add</button>
     </form>
   )
